00805bb192f05ba7b64f6652566d27b0a39ba69a
mp3 <im.in.a.coma.xxx@gmail.com>
2019-05-07 18:47:41 +0900

    drag and drop

--- src/components/Dissolver.tsx
+++ src/components/Dissolver.tsx
@@ -2,17 +2,40 @@ import * as React from 'react'
 import styled from 'styled-components'
 
 export default () => {
+  const [image, setImage] = React.useState(null)
+
+  const fileReader = new FileReader()
+  fileReader.onload = (event: any) => {
+    const result = event.target.result
+    setImage(result)
+  }
+
+  const checkDraggedFile = (event: React.DragEvent) => {
+    event.preventDefault()
+
+    const files = event.dataTransfer.files
+    Array.from(files).forEach(file => {
+      if (file.type.includes('image/')) {
+        fileReader.readAsDataURL(file)
+      }
+    })
+  }
+
   return (
     <Container>
-      <DraggableArea />
+      <Image src={image || ''} />
+      <DraggableArea onDragOver={checkDraggedFile} onDrop={checkDraggedFile} />
     </Container>
   )
 }
 
 const Container = styled.div`
-  padding: 10px;
-  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
+
+const Image = styled.img``
 
 const DraggableArea = styled.div`
   position: fixed;
